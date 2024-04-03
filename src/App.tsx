62049382d06d68e0b0d41da98ca3e5eeb0f76253
STHEXA <53681446+STHEXA@users.noreply.github.com>
2024-04-03 22:17:36 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <>
+      <ChakraProvider>
+        <Button colorScheme="teal">ボタン</Button>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
